77d78f5701e05c068c0d7b613d0ded463e397111
c0dezer019 <c0dezer019@bblanke nship.me>
2021-07-07 11:59:52 -0500

    A pie graph.

---
+++ src/components/atoms/Pie.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { View } from 'react-native';
+
+import { useAppSelector } from '../../redux/app/hooks';
+
+const Pie = (): JSX.Element => {
+  return <View />;
+};
+
+export default Pie;
