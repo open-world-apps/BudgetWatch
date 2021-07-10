24a02da0d8fe5b836e06e3172087d50b3a411927
c0dezer019 <c0dezer019@bblanke nship.me>
2021-07-09 21:49:50 -0500

    State updates.

--- src/redux/reducers/balanceSlice.ts
+++ src/redux/reducers/balanceSlice.ts
@@ -2,40 +2,50 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState, AppThunk } from '../app/store';
 
 export interface BalanceState {
-  remaining: number;
-  target: number;
+   remaining: number;
+   target: number;
+   accounts: number;
 }
 
 const initialState: BalanceState = {
-  remaining: 0,
-  target: 0,
+   remaining: 0,
+   target: 0,
+   accounts: 0, // How many accounts the user has stored.
 };
 
 const balanceSlice = createSlice({
-  name: 'balance',
-  initialState,
-  reducers: {
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.remaining += action.payload;
-    },
-    decrementByAmount: (state, action: PayloadAction<number>) => {
-      state.remaining -= action.payload;
-    },
-    increaseTarget: (state, action: PayloadAction<number>) => {
-      state.target += action.payload;
-    },
-    decreaseTarget: (state, action: PayloadAction<number>) => {
-      state.target -= action.payload;
-    },
-  },
-  extraReducers: builder => {},
+   name: 'balance',
+   initialState,
+   reducers: {
+      incrementByAmount: (state, action: PayloadAction<number>) => {
+         state.remaining += action.payload;
+      },
+      decrementByAmount: (state, action: PayloadAction<number>) => {
+         state.remaining -= action.payload;
+      },
+      increaseTarget: (state, action: PayloadAction<number>) => {
+         state.target += action.payload;
+      },
+      decreaseTarget: (state, action: PayloadAction<number>) => {
+         state.target -= action.payload;
+      },
+      incrementAccounts: (state, action: PayloadAction<number>) => {
+         state.accounts++;
+      },
+      decrementAccounts: (state, action: PayloadAction<number>) => {
+         state.accounts--;
+      },
+   },
+   extraReducers: builder => {},
 });
 
 export const {
-  incrementByAmount,
-  decrementByAmount,
-  increaseTarget,
-  decreaseTarget,
+   incrementByAmount,
+   decrementByAmount,
+   increaseTarget,
+   decreaseTarget,
+   incrementAccounts,
+   decrementAccounts,
 } = balanceSlice.actions;
 
 export default balanceSlice.reducer;
--- src/redux/reducers/budgetSlice.ts
+++ src/redux/reducers/budgetSlice.ts
@@ -1,45 +1,54 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState, AppThunk } from '../app/store';
 
 type Percentage = number;
 
 export interface BudgetState {
-  remaining: number;
-  target: number | Percentage;
-  measurement: string;
+   remaining: number;
+   target: number | Percentage;
+   measurement: string;
+   budgets: number;
 }
 
 const initialState: BudgetState = {
-  remaining: 0,
-  target: 0,
-  measurement: 'fixed',
+   remaining: 0,
+   target: 0,
+   measurement: 'fixed',
+   budgets: 0, // How many budgets the user has stored.
 };
 
 const budgetSlice = createSlice({
-  name: 'budget',
-  initialState,
-  reducers: {
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.remaining += action.payload;
-    },
-    decrementByAmount: (state, action: PayloadAction<number>) => {
-      state.remaining -= action.payload;
-    },
-    increaseTarget: (state, action: PayloadAction<number>) => {
-      state.target += action.payload;
-    },
-    decreaseTarget: (state, action: PayloadAction<number>) => {
-      state.target -= action.payload;
-    },
-  },
-  extraReducers: builder => {},
+   name: 'budget',
+   initialState,
+   reducers: {
+      incrementByAmount: (state, action: PayloadAction<number>) => {
+         state.remaining += action.payload;
+      },
+      decrementByAmount: (state, action: PayloadAction<number>) => {
+         state.remaining -= action.payload;
+      },
+      incrementBudgets: (state, action: PayloadAction<number>) => {
+        state.budgets++;
+      },
+      decrementBudgets: (state, action: PayloadAction<number>) => {
+        state.budgets--;
+      },
+      increaseTarget: (state, action: PayloadAction<number>) => {
+         state.target += action.payload;
+      },
+      decreaseTarget: (state, action: PayloadAction<number>) => {
+         state.target -= action.payload;
+      },
+   },
+   extraReducers: builder => {},
 });
 
 export const {
-  incrementByAmount,
-  decrementByAmount,
-  increaseTarget,
-  decreaseTarget,
+   incrementByAmount,
+   decrementByAmount,
+   incrementBudgets,
+   decrementBudgets,
+   increaseTarget,
+   decreaseTarget,
 } = budgetSlice.actions;
 
 export default budgetSlice.reducer;
