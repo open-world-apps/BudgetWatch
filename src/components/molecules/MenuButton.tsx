3a1e1ff44df9f672bff232067bb8ed240f946812
c0dezer019 <c0dezer019@bblanke nship.me>
2021-07-15 21:21:24 -0500

    Button for menu.

---
+++ src/components/molecules/MenuButton.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement, useState } from 'react';
+
+import { GestureResponderEvent, Pressable, Text, View } from 'react-native';
+
+const MenuButton = (): ReactElement => {
+   const [isOpen, openMenu] = useState<boolean>(false);
+
+   return (
+      <Pressable>
+         <View />
+         <View />
+      </Pressable>
+   );
+};
