c14d93a0637f35abd1dd0303d4c634f52eff5904
c0dezer019 <c0dezer019@bblanke nship.me>
2021-08-02 21:01:20 -0500

    Updates.

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
-export { default } from './storybook';
+export { default } from './src/storybook';
 
 AppRegistry.registerComponent(appName, () => App);
