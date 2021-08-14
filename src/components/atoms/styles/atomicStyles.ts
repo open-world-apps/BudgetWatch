66d36da69ad4d058566707ec58be9eb32cf2ae5d
c0dezer019 <c0dezer019@bblanke nship.me>
2021-08-14 01:28:22 -0500

    Formatting.

--- src/components/atoms/styles/atomicStyles.ts
+++ src/components/atoms/styles/atomicStyles.ts
@@ -1,19 +1,21 @@
-import { StyleProp, StyleSheet, TextStyle, ViewStyle } from 'react-native';
+import { StyleSheet, TextStyle, ViewStyle } from 'react-native';
 
 export interface Styles {
-   label: TextStyle;
-   pressable?: ViewStyle;
-   text: TextStyle;
+  label: TextStyle;
+  pressable?: ViewStyle;
+  text: TextStyle;
 }
 
 export const styles = StyleSheet.create<Styles>({
-   text: {
-      textAlign: 'center',
-      fontFamily: 'Inconsolata-Bold',
-      fontSize: 25,
-      height: 28,
-   },
-   label: {
-      fontSize: 13,
-   },
+  text: {
+    textAlign: 'center',
+    fontFamily: 'Inconsolata-Bold',
+    fontSize: 25,
+    height: 28,
+  },
+  label: {
+    fontFamily: 'Inconsolata-Bold',
+    fontSize: 13,
+    height: 13,
+  },
 });
