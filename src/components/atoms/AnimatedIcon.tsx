bc3da80ade6e0d8de34c8f6328de3d8e166b252e
c0dezer019 <c0dezer019@bblanke nship.me>
2021-08-14 01:26:16 -0500

    Formatting.

--- src/components/atoms/AnimatedIcon.tsx
+++ src/components/atoms/AnimatedIcon.tsx
@@ -1,31 +1,31 @@
 import React, { ReactElement, useRef } from 'react';
-import { View, Animated } from 'react-native';
+import { Animated } from 'react-native';
 import Svg, { Line } from 'react-native-svg';
 
 const AnimatedIcon = (): ReactElement => {
-   const rotAnim = useRef(new Animated.Value(0)).current;
+  const rotAnim = useRef(new Animated.Value(0)).current;
 
-   const rotateOpen = () => {
-      Animated.timing(rotAnim, {
-         toValue: 180,
-         duration: 1000,
-         useNativeDriver: true
-      }).start();
-   };
-   
-   const rotateClose = () => {
-      Animated.timing(rotAnim, {
-         toValue: 0,
-         duration: 1000,
-         useNativeDriver: true
-      }).start();
-   };
+  const rotateOpen = () => {
+    Animated.timing(rotAnim, {
+      toValue: 180,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  };
 
-   return (
-      <Svg height="20" width="20">
-         <Line x1="0" y1="0" x2="20" y2="20" stroke="blue" strokeWidth="2" />
-      </Svg>
-   );
+  const rotateClose = () => {
+    Animated.timing(rotAnim, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  return (
+    <Svg height="20" width="20">
+      <Line x1="0" y1="0" x2="20" y2="20" stroke="blue" strokeWidth="2" />
+    </Svg>
+  );
 };
 
 export default AnimatedIcon;
