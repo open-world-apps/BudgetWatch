b65b2b2001a70b71c289797fdf6397a86680a969
c0dezer019 <c0dezer019@bblanke nship.me>
2021-10-14 11:58:32 -0500

    Initial commit.
    The home view of the app.

---
+++ src/components/screens/Main.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+import { View } from 'react-native';
+
+interface Props {
+  navigation: any;
+}
+
+const Main = ({ navigation }: Props): ReactElement => <View />;
+
+export default Main;
