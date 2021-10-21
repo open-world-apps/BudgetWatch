9ce2fedd57d56045cb487bdbf45912bd49c9599f
c0dezer019 <c0dezer019@bblanke nship.me>
2021-10-21 13:01:23 -0500

    Created a util function for setup.

--- src/utils/dataOps.ts
+++ src/utils/dataOps.ts
@@ -1,23 +1,5 @@
-import { generator } from './colors';
-import { useAppSelector } from '../redux/app/hooks';
-import type { DataSet } from '../components/atoms/pieChart/Pie';
+const fs = require('fs');
 
-interface Parameters {
-  balance: number;
-  budget: string;
-  color?: string;
-}
+const dir = '../components/organisms/FirstStepsModal';
 
-const createDataSet = (data: Parameters) => {
-  const count = useAppSelector(state => state.budget.budgets);
-  const colors = generator(count, 'hex');
-  const dataArr = new Array(count);
-
-  return dataArr.map((el, i) => {
-    el = {
-      balance: data.balance,
-      budget: data.budget,
-      color: colors[i],
-    };
-  });
-};
+export const stepCounter = () => fs.readdirSync(dir);
