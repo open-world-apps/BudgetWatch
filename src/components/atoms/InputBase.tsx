8ff11c12206f7133d99e792ea0d5d2ae084be381
c0dezer019 <c0dezer019@bblanke nship.me>
2021-10-25 15:52:12 -0500

    Formatting and adding PropTypes.

--- src/components/atoms/InputBase.tsx
+++ src/components/atoms/InputBase.tsx
@@ -4,29 +4,45 @@ import PropTypes from 'prop-types';
 import { KeyboardTypeOptions, TextInput } from 'react-native';
 
 interface Input {
-    keyboardType: KeyboardTypeOptions;
-    onChangeText?: () => {};
-    placeholder: string;
-    style: Object;
-    value?: any;
+  keyboardType: KeyboardTypeOptions;
+  onChangeText?: () => {};
+  placeholder: string;
+  style: Object;
+  value?: any;
 }
 
-const InputBase = ({ keyboardType, onChangeText, placeholder, style, value }: Input) => {
-    const [fieldStyle, setStyle] = useState({});
+const InputBase = ({
+  keyboardType,
+  onChangeText,
+  placeholder,
+  style,
+  value,
+}: Input) => {
+  const [fieldStyle, setStyle] = useState({});
 
-    useEffect(() => {
-        setStyle(style);
-    }, [style]);
+  useEffect(() => {
+    setStyle(style);
+  }, [style]);
 
-    return (
-        <TextInput
-            keyboardType={keyboardType}
-            onChangeText={onChangeText}
-            placeholder={placeholder}
-            style={fieldStyle}
-            value={value}
-        />
-    )
+  return (
+    <TextInput
+      keyboardType={keyboardType}
+      onChangeText={onChangeText}
+      placeholder={placeholder}
+      style={fieldStyle}
+      value={value}
+    />
+  );
+};
+
+InputBase.propTypes = {
+  onChangeText: PropTypes.func,
+  value: PropTypes.any,
+};
+
+InputBase.defaultProps = {
+  onChangeText: () => {},
+  value: '',
 };
 
 export default InputBase;
